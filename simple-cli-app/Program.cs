34898bc8597a1bf2535423aef1b8ad3ceb47de65
rorychatt <pavel.rinne@gmail.com>
2024-07-08 09:43:12 +0200

    Feat: app class init

--- simple-cli-app/Program.cs
+++ simple-cli-app/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class App
+{
+    public class ReadableData
+    {
+        public required string Name { get; set; }
+    }
+    public static List<ReadableData> ReadJSONFile(string url)
+    {
+        string jsonString = File.ReadAllText(url);
+        List<ReadableData> data = JsonSerializer.Deserialize<List<ReadableData>>(jsonString);
+        return data;
+    }
+
+    public static void WriteToJSONFile(string url, List<ReadableData> data)
+    {
+        JsonSerializerOptions options = new()
+        {
+            WriteIndented = true
+        };
+        string jsonString = JsonSerializer.Serialize(data, options);
+        File.WriteAllText(url, jsonString);
+    }
+}
